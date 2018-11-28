05f416f4e671c4ef658371bdd68f27cedc366535
realzhangm <realzhangm@163.com>
2018-11-29 00:06:54 +0800

    add link list

---
+++ leetcode/README.md
@@ -0,0 +1,6 @@
+#### Summary
+
+| # | Title | Solution |
+|---| ----- | -------- |
+|2|[Add Two Numbers](https://leetcode-cn.com/problems/add-two-numbers/) | [C++](./link_list/0002_Add_Two_Numbers/main.cpp)|
+
--- leetcode/link_list/merge_two_sorted_list.cpp
+++ leetcode/link_list/merge_two_sorted_list.cpp
@@ -9,6 +9,7 @@
 //输出：1->1->2->3->4->4
 
 #include "../leetcode.h"
+#include "listnode.h"
 
 /**
  * Definition for singly-linked list.
@@ -19,33 +20,7 @@
  * };
  */
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
 
-    static ListNode* MakeList(vector<int> nums) {
-        ListNode* p = nullptr;
-        ListNode* h = nullptr;
-        for (auto i = 0; i < nums.size(); i++) {
-            ListNode* l = new ListNode(nums[i]);
-            if (i == 0) {
-                h = p = l;
-            }
-            p->next = l;
-            p = l;
-        }
-        return h;
-    }
-
-    static void PrintList(ListNode* l) {
-        while (l) {
-            cout << l->val << " ";
-            l = l->next;
-        }
-        cout << endl;
-    }
-};
 
 class Solution {
 public:
