38a9562aca0a5ab58bc3e4ac3d0344fc38152572
realzhangm <realzhangm@163.com>
2018-12-16 11:13:27 +0800

    add 0208_Implement_Trie

--- leetcode/design/0208_Implement_Trie/main.cpp
+++ leetcode/design/0208_Implement_Trie/main.cpp
@@ -23,24 +23,151 @@
 
 class Trie {
 public:
+    class Node {
+        public:
+            Node() {
+                is_word_ = false;
+            }
+            Node* FindNext(char c) {
+                if (next_.find(c) == next_.end()) {
+                    return nullptr;
+                }
+                return next_[c];
+            }
+            void InsertNext(char c, Node* n) {
+                next_.insert(make_pair(c, n));
+            }
+            bool IsWord() {
+                return is_word_;
+            }
+            void SetWord() {
+                is_word_ = true;
+            }
+        private:
+            bool is_word_;
+            unordered_map<char, Node*> next_;
+    };
+
     /** Initialize your data structure here. */
     Trie() {
-
+        root_ = new Node;
     }
 
     /** Inserts a word into the trie. */
     void insert(string word) {
-
+        if (word.size() == 0) return;
+        Node* curr = root_;
+        Node* next = nullptr;
+        for (auto &c : word) {
+            next = curr->FindNext(c);
+            if (next == nullptr) {
+                next = new Node();
+                curr->InsertNext(c, next);
+            }
+            curr = next;
+        }
+        if (!curr->IsWord()) {
+            curr->SetWord();
+        }
     }
 
     /** Returns if the word is in the trie. */
     bool search(string word) {
-
+        if (word.size() == 0) return false;
+        Node* curr = root_;
+        Node* next = nullptr;
+        for (auto &c : word) {
+            next = curr->FindNext(c);
+            if (next == nullptr) {
+                return false;
+            }
+            curr = next;
+        }
+        return curr->IsWord();
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
+        if (prefix.size() == 0) return false;
+        Node* curr = root_;
+        Node* next = nullptr;
+        for (auto &c : prefix) {
+            next = curr->FindNext(c);
+            if (next == nullptr) {
+                return false;
+            }
+            curr = next;
+        }
+        return true;
+    }
 
+private:
+    Node* root_;
+};
+
+class Trie2 {
+
+private:
+    struct Node{
+        map<char, int> next;
+        bool end = false;
+    };
+    vector<Node> trie;
+
+public:
+    Trie2() {
+        trie.clear();
+        trie.push_back(Node());
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(const string& word) {
+        int treeID = 0;
+        for (char c: word){
+            if (trie[treeID].next.find(c) == trie[treeID].next.end()) {
+                trie[treeID].next[c] = trie.size();
+                trie.push_back(Node());
+            }
+
+            treeID = trie[treeID].next[c];
+        }
+
+        trie[treeID].end = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(const string& word){
+        int treeID = 0;
+        for (char c: word) {
+            if (trie[treeID].next.find(c) == trie[treeID].next.end())
+                return false;
+
+            treeID = trie[treeID].next[c];
+        }
+        return trie[treeID].end;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(const string& prefix) {
+
+        int treeID = 0;
+        for (char c: prefix) {
+            if (trie[treeID].next.find(c) == trie[treeID].next.end())
+                return false;
+
+            treeID = trie[treeID].next[c];
+        }
+        return true;
     }
 };
 
+int main() {
+    Trie* trie = new Trie;
+    trie->insert("apple");
+    cout << trie->search("appl");   // 返回 true
+    cout << endl << trie->search("app");     // 返回 false
+    cout << endl << trie->startsWith("app"); // 返回 true
+    trie->insert("app");
+    cout << endl <<  trie->search("app");     // 返回 true
+
+}
--- leetcode/leetcode.h
+++ leetcode/leetcode.h
@@ -10,6 +10,7 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <map>
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
