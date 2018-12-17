1af88778397814b0e62831f280a0cb23d040940a
realzhangm <realzhangm@163.com>
2018-12-17 23:13:24 +0800

    93.复原IP地址, 优化剪枝

--- leetcode/backtracking/0093_Restore_IP_Addresses/main.cpp
+++ leetcode/backtracking/0093_Restore_IP_Addresses/main.cpp
@@ -20,21 +20,30 @@ public:
         FindIpAddresses(s, 0, ip, res);
         return res;
     }
+
 private:
-    void FindIpAddresses(string s, int index, vector<string>& ip, vector<string>& res) {
+    void FindIpAddresses(string s, int index, vector<string> &ip, vector<string> &res) {
         if (index == s.size()) {
             if (ip.size() == 4) {
-                string ip_addr = ip[0]+"."+ip[1]+"."+ip[2]+"."+ip[3];
+                string ip_addr = ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3];
                 res.push_back(ip_addr);
             }
             return;
         }
         for (int step = 1; step <= 3; step++) {
+            if (12 - ip.size() * 3 < s.size() - index) {
+                cout << "😂😂😂😂" << endl;
+                break;
+            }
+            if (index + step > s.size()) {
+                break;
+            }
             string ip_addr_qtr = s.substr(index, step);
-            if (index + step > s.size()) break;
-            if (ip_addr_qtr.size() > 1 && ip_addr_qtr[0] == '0') continue;
+            if (ip_addr_qtr.size() > 1 && ip_addr_qtr[0] == '0') {
+                continue;
+            }
             int ip_qtr_num = stoi(ip_addr_qtr);
-            if (ip_qtr_num >= 0 && ip_qtr_num <=255) {
+            if (ip_qtr_num >= 0 && ip_qtr_num <= 255) {
                 ip.push_back(ip_addr_qtr);
                 FindIpAddresses(s, index + step, ip, res);
                 ip.pop_back();
